8f3dd7f04e91d92a1181890e080c672391d5358e
kwokho74 <kwokho@kwokho.com>
2022-09-04 08:48:08 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
